6bd443b7aefcbffb43ef3006f95fe9a46cf8b0df
Jo Bub <jo@jostuff.de>
2024-09-11 22:34:17 +0200

    FuckingTextWorks

--- Assets/Scripts/MenueUiHandler.cs
+++ Assets/Scripts/MenueUiHandler.cs
@@ -1,21 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using System.Collections;
+using TMPro;
 
 public class MenueUiHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+{   static string inputname;
+    GameObject errorMessage;
+
+    private void Start() {
+        errorMessage = GameObject.Find("NeedToEnterNAmeText");
+        DisableFuckingText();
     }
 
     public void StartNew(){
-        SceneManager.LoadScene(1);
+        if (inputname != null){
+            SceneManager.LoadScene(1);}
+        else{
+            errorMessage.GetComponent<TextMeshProUGUI>().enabled = true;
+            StartCoroutine(ErrorTimer());
+        }
+    }
+
+    public void DisableFuckingText(){
+        errorMessage.GetComponent<TextMeshProUGUI>().enabled = false;
     }
 
     public void Exit(){
-
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
@@ -23,9 +34,17 @@ public class MenueUiHandler : MonoBehaviour
 #endif
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ReadName(string s){
+        inputname = s;
+        Debug.Log("The Name is: " + inputname);
     }
+
+    IEnumerator ErrorTimer()
+    {
+        yield return new WaitForSeconds(3);
+        DisableFuckingText();
+    }
+
+
+
 }
